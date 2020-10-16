d6a606281c4c441e5163f8a752a71abb59deb30a
NathanLeMmon <68714005+NathanLeMmon@users.noreply.github.com>
2020-10-15 20:37:54 -0600

    Create Salt.cs

---
+++ Assignment2TypingGame/Assignment2TypingGame/Pages/LogIn/Salt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Assignment2TypingGame.Pages.LogIn
+{
+    public class Salt
+    {
+        public string salt()
+        {
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            byte[] buffer = new byte[1024];
+
+            rng.GetBytes(buffer);
+            string salt = BitConverter.ToString(buffer);
+            return salt;
+        }
+    }
+}
