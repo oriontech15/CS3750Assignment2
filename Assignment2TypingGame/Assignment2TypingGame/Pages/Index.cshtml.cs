aac0a7ef2581e4283a0fd467b33f2b8a4611f179
madisonjones515@gmail.com <madisonjones515@gmail.com>
2020-10-29 20:26:46 -0600

    Database working with salt

--- Assignment2TypingGame/Assignment2TypingGame/Pages/Index.cshtml.cs
+++ Assignment2TypingGame/Assignment2TypingGame/Pages/Index.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Assignment2TypingGame.Data;
+using Assignment2TypingGame.Pages.LogIn;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore.Infrastructure;
@@ -19,6 +20,7 @@ namespace Assignment2TypingGame.Pages
             _unitOfWork = unitOfwork;
         }
 
+        Salt salt = new Salt();
 
         [BindProperty]
         public User.User UserObj { get; set; }
@@ -43,6 +45,9 @@ namespace Assignment2TypingGame.Pages
 
         public IActionResult OnPost()
         {
+
+            UserObj.Salt = salt.salt();
+
             if (!ModelState.IsValid)
             {
                 return Page();
