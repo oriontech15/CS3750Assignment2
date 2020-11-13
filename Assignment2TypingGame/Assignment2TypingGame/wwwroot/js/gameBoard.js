8c930cd7ef1438e738b900f4cb43739432fafa9d
madisonjones515@gmail.com <madisonjones515@gmail.com>
2020-11-12 21:14:54 -0700

    Added countdown timer to game page

--- Assignment2TypingGame/Assignment2TypingGame/wwwroot/js/gameBoard.js
+++ Assignment2TypingGame/Assignment2TypingGame/wwwroot/js/gameBoard.js
@@ -1,4 +1,9 @@
-﻿const words = <% this.#Assignment2TypingGame.Csharp.GetWords(1) %>
+﻿//const words = <% this.#Assignment2TypingGame.Csharp.GetWords(1) %>
+var seconds = 60;
+
+$(document).ready(function () {
+    startGame();
+});
 
 function JSgetWords() {
     return words;
@@ -6,4 +11,32 @@ function JSgetWords() {
 
 function getNextWord() {
     return words[++words.current]
+}
+
+//This function is called when the start game button is clicked
+function startGame() {
+    
+    startClock();
+    runClock();
+}
+
+function startClock() {
+    clockId = setInterval(runClock, 1000);
+    //This will have the runClock method call every 1 second
+}
+
+//This method will reduce the seconds variable by 1 every second and display it to the screen.
+function runClock() {
+
+    seconds -= 1;
+    // This will decrement the seconds variable by 1
+    document.getElementById("timer").setAttribute("value", seconds);
+    // This will set the timer element to what is being seen in the seconds variable
+    if (seconds == 0) {
+        stopClock();
+    }
+}
+
+function stopClock() {
+    clearInterval(clockId)
 }
