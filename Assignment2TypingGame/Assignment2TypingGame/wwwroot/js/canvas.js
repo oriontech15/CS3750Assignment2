95e74aeceef1bad6c2d47494d5da51543feac1cb
NathanLeMmon <68714005+NathanLeMmon@users.noreply.github.com>
2020-11-12 20:53:58 -0700

    canvas stuff

---
+++ Assignment2TypingGame/Assignment2TypingGame/wwwroot/js/canvas.js
@@ -0,0 +1,34 @@
+﻿<script>
+
+    function init() {
+
+        gameLoop()
+    }
+
+let rectX = 0;
+let rectY = 0;
+var canvas = document.getElementById("canvasPlayer1");
+var canvas = document.getElementById("canvasPlayer2");
+let context = canvas.getContext("2d");
+
+function draw() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+    context.fillStyle = '#ff8080';
+    context.fillRect(rectX, rectY, 50, 25);
+}
+
+function gameLoop(timeStamp) {
+        // Update game objects in the loop
+        update();
+    draw();
+
+    window.requestAnimationFrame(gameLoop);
+}
+
+function update() {
+        rectY += 1;
+}
+
+init();
+</script>
