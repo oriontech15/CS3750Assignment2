eed937abe37ba4d62d834e9f1ed66394886c5351
madisonjones515@gmail.com <madisonjones515@gmail.com>
2020-11-21 22:42:29 -0700

    signalR is working with player input

--- Assignment2TypingGame/Assignment2TypingGame/Models/GameState.cs
+++ Assignment2TypingGame/Assignment2TypingGame/Models/GameState.cs
@@ -8,23 +8,7 @@ namespace Assignment2TypingGame.Models.GameState
         public string name {get; set;}
         public string currentInput {get; set;}
         public int score {get; set;}
-
-        // Word Lists
-        //public List<string> wordList {get; set;}
         public List<string> currentWordList {get; set;}
-        //public List<string> completedWordList {get; set;}
-
         public List<Dictionary<string, int>> positions {get; set;}
-
-        public GameState()
-        {
-            // var gameState = new JavaScriptSerializer().Deserialize<GameState>(json);
-            // gameState.name // == json["name"]
-        }
-
-        //public IActionResult Get() {
-
-        //    return JSON (new JavaScriptSerializer().Deserialize<GameState>(rawJSON););
-        //}
     }
 }
