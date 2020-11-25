7126cfd66bfdcff7567d00e5e7a9ad79fb80fd3e
Justin <oriontech15@gmail.com>
2020-11-24 19:42:18 -0700

    minor polish with bug fixes and requirement updates

--- Assignment2TypingGame/Assignment2TypingGame/wwwroot/js/canvas.js
+++ Assignment2TypingGame/Assignment2TypingGame/wwwroot/js/canvas.js
@@ -2,7 +2,7 @@
 /**** SETUP DEFAULT VALUES ****/
 /******************************/
 
-var words = ["word", "the", "be", "to", "of", "and", "in", "that", "have", "it", "for", "not", "on", "with", "he", "as", "you", "do", "at", "this", "but", "his", "by", "from", "they", "we", "say", "her", "she", "or", "an", "will", "my", "one", "all", "would", "there", "their", "what", "so", "up", "out", "if", "about", "who", "get", "which", "go", "me", "when", "make", "can", "like", "time", "no", "just", "him", "know", "take", "people", "into", "year", "your", "good", "some", "could", "them", "see", "other", "than", "then", "now", "look", "only", "come", "its", "over", "think", "also", "back", "after", "use", "two", "how", "our", "work", "first", "well", "way", "even", "new", "want", "because", "any", "these", "give", "day", "most", "us"]
+var words = ["backyard", "back", "backup", "backstroke", "backstreet", "word", "the", "be", "to", "of", "and", "in", "that", "have", "it", "for", "not", "on", "with", "he", "as", "you", "do", "at", "this", "but", "his", "by", "from", "they", "we", "say", "her", "she", "or", "an", "will", "my", "one", "all", "would", "there", "their", "what", "so", "up", "out", "if", "about", "who", "get", "which", "go", "me", "when", "make", "can", "like", "time", "no", "just", "him", "know", "take", "people", "into", "year", "your", "good", "some", "could", "them", "see", "other", "than", "then", "now", "look", "only", "come", "its", "over", "think", "also", "back", "after", "use", "two", "how", "our", "work", "first", "well", "way", "even", "new", "want", "because", "any", "these", "give", "day", "most", "us"]
 var currentWords = [];
 var wordIndex = 0;
 
@@ -218,7 +218,7 @@ function drawPlayer2Canvas(word1, word2, word3, word4, word5) {
 
     if (compareInput(word5, player2CurrentInput)) {
         context2.fillStyle = highlightColor;
-        context2.fillText(input, (wordBoxWidth * 4) + padding + 25, player2Word5Y, wordBoxWidth);
+        context2.fillText(player2CurrentInput, (wordBoxWidth * 4) + padding + 25, player2Word5Y, wordBoxWidth);
     }
 }
 
